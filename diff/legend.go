429b47587e36498f8fc43437a50342d1cd93d898
yanzihuan <yanzihuan@bytedance.com>
2021-08-29 13:30:27 +0800

    feat: v1.0.0

---
+++ diff/legend.go
@@ -0,0 +1,37 @@
+// Package diff 图例
+package diff
+
+import "strings"
+
+const (
+	MarkDelete = "---" // 源文件图例
+	MarkInsert = "+++" // 目标文件图例
+)
+
+// Legend diff图例
+type Legend struct {
+	Mark    string `json:"mark,omitempty"`
+	Content string `json:"content,omitempty"`
+}
+
+// NewLegend 创建diff图例实例
+func NewLegend() *Legend {
+	return &Legend{}
+}
+
+// IsLegend 是否为diff图例起始行
+func IsLegend(diffLine string) bool {
+	return strings.HasPrefix(diffLine, MarkDelete) || strings.HasPrefix(diffLine, MarkInsert)
+}
+
+// Parse 解析diff图例
+func (l *Legend) Parse(diffLine string) *Legend {
+	if len(diffLine) < 4 {
+		return nil
+	}
+
+	l.Mark = diffLine[:3]
+	l.Content = diffLine[4:]
+
+	return l
+}
---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/lower-coder/go-git-diff
+
+go 1.16
+
+require github.com/spf13/cast v1.4.1
